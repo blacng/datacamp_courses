51a87c8f0471e79e4a0eb5190e8ce825c79c709a
blacng <blacng@gmail.com>
2019-06-18 22:34:17 -0400

    adding writing functions rmd and html

--- atlanta_temp_revised.R
+++ atlanta_temp_revised.R
@@ -7,16 +7,16 @@ library(lubridate)
 library(scales)
 library(here)
 
-#Read data
-tempdata <- read.table("https://d37djvu3ytnwxt.cloudfront.net/assets/courseware/v1/592f3be3e90d2bdfe6a69f62374a1250/asset-v1:GTx+ISYE6501x+3T2017+type@asset+block/temps.txt", header = T) 
+# Read data
+tempdata <- read.table("https://d37djvu3ytnwxt.cloudfront.net/assets/courseware/v1/592f3be3e90d2bdfe6a69f62374a1250/asset-v1:GTx+ISYE6501x+3T2017+type@asset+block/temps.txt", header = T)
 
-#Remove 'X' in the variable names
+# Remove 'X' in the variable names
 colnames(tempdata) <- str_replace(colnames(tempdata), "X", "")
 
 # check
 glimpse(tempdata)
 
-##Use tidyr() to restructure temp_df
+## Use tidyr() to restructure temp_df
 tidy_temp_atlanta <- gather(tempdata, YEAR, TEMP, -DAY)
 
 # set day to date:
@@ -24,7 +24,7 @@ tidy_temp_atlanta$DAY <- as.Date(tidy_temp_atlanta$DAY, format = "%d-%b")
 
 glimpse(tidy_temp_atlanta)
 
-##Plot geom_line with facet
+## Plot geom_line with facet
 ggplot(tidy_temp_atlanta, aes(DAY, TEMP, group = 1)) +
   geom_line(colour = "blue") +
   facet_wrap(~YEAR, ncol = 4)
